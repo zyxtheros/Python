c8de0fcfb284bbf39f4e304cfb76425c9c37c5fc
Capps <cappsc@mail.gvsu.edu>
2022-03-14 13:09:59 -0400

    commit class for player

--- intro to motion graphics/main.py
+++ intro to motion graphics/main.py
@@ -2,29 +2,42 @@
 import turtle
 import time
 
-# GLOBAL VARIABLES
+### GLOBAL VARIABLES ###
 
 win = turtle.Screen() # create an instance of a screen
 win.title("ANIMATION DEMO") # window title
 win.bgcolor("black") # set the background color
 
-player = turtle.Turtle() # create a new entity
-player.shape("square") # set the entity shape
-player.color("green") # set the entity color
+# Register shapes
+win.register_shape("invader.gif")
+win.register_shape("invader2.gif")
 
+class Player(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.penup()
+        self.shape("invader.gif")  # set the entity shape
+        self.color("green")  # set the entity color
+        self.frame = 0  # set the initial frame number
+        self.frames = ["invader.gif", "invader2.gif"]
 
+    def animate(self):
+        self.frame += 1
+        if self.frame >= len(self.frames):
+            self.frame = 0  # reset the frame count
+        self.shape(self.frames[self.frame])
+
+        # Set timer for time between each frame
+        win.ontimer(self.animate, 500)
+
+player = Player() # create a new entity instance
+player2 = Player()
+player2.goto(-100, 0)
 
 # RUN CONDITIONS
-def player_animate():
-    if player.shape() == "square":
-        player.shape("circle")
-    elif player.shape() == "circle":
-        player.shape("square")
 
-    # Set timer
-    win.ontimer(player_animate, 500)
-
-player_animate()
+player2.animate()
+player.animate()
 
 while True:
     win.update() # pause before updating
