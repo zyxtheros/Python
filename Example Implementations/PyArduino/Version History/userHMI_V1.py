464aca73dc6c6a9a0dc30db2afb372e6329364c1
Capps <cappsc@mail.gvsu.edu>
2022-06-12 18:47:22 -0400

    added PyArduino version 2

--- Example Implementations/PyArduino/Version History/userHMI_V1.py
+++ Example Implementations/PyArduino/Version History/userHMI_V1.py
@@ -9,13 +9,33 @@ def clock():
     while True:
         sleep(1)
         getSerial()
+
+        @SerialParse_csv
+        getSerial()
+
         root.update_idletasks() # request a window update
         clock # run on a continuous cycle
+"""
+    Parse input data separated by commas into usable float values
+    decorator for the getSerial function
+"""
+def SerialParse_csv(func):
+    def inner():
+        func()
+        splitPacket = dataPacket.split(", ") # Tokenize the string into individual numbers
+        for item in range(0, len(splitPacket)): # convert the packet items to floats
+            splitPacket[item] = float(splitPacket[item])
+        print(splitPacket+1)
 
+    return splitPacket
+
+    
+
+@SerialParse_csv
 def getSerial():
     dataPacket = arduinoData.readline() # Get the COM data sent from the arduino
-    dataPacket = str(dataPacket, 'utf-8') # convert the bytes back to string to remove secape chars
-    dataPacket = dataPacket.strip('\r\n') # remove the extra reutrn from the decoding
+    dataPacket = str(dataPacket, 'utf-8') # convert the bytes back to string to remove escape chars
+    dataPacket = dataPacket.strip('\r\n') # remove the extra return from the decoding
     my_label1.config(text=dataPacket)
 
 root = Tk()
@@ -24,6 +44,8 @@ root.geometry("500x400")
 
 my_label1 = Label(root, text = "(X,Y,Z)")
 my_label1.pack(pady = 20)
+my_label2 = Label(root, text = "(X+1,Y+1,Z+1)")
+my_label2.pack(pady = 20)
 
 thread_clock = threading.Thread(target = clock)
 thread_getSerial = threading.Thread(target = getSerial)
