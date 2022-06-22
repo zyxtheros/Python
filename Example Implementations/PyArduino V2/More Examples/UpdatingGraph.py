a773490fd515be5220a268fe5401ada5a7accecc
calebcapps <caleb.capps@gentex.com>
2022-06-22 15:45:37 -0400

    add interactive graphs example and cleaned up live graphs

--- Example Implementations/PyArduino V2/More Examples/UpdatingGraph.py
+++ Example Implementations/PyArduino V2/More Examples/UpdatingGraph.py
@@ -4,30 +4,33 @@ from dash import dcc, html
 import plotly
 import random
 import plotly.graph_objs as go
-from collections import deque
+from collections import deque # allows for creating an array with a constant size that we can update to
 import os
 import webbrowser
 
-X = deque(maxlen=20)
-X.append(1)
-Y = deque(maxlen=20)
+X = deque(maxlen=20) # array of x coordinates
+Y = deque(maxlen=20) # array of y coordinates
+
+X.append(1) # start off with initial set to 1 for x and y
 Y.append(1)
 
 
 app = dash.Dash(__name__)
 app.layout = html.Div(
     [
-        dcc.Graph(id='live-graph', animate=True),
+        dcc.Graph(id='live-graph', animate=True), # this is the actual graph element
         dcc.Interval(
-            id='graph-update',
-            interval=1000,
+            id='graph-update',  # what we are updating
+            interval=1000,      # How fast the interval will occur
             n_intervals = 0
         ),
     ]
 )
 
-@app.callback(Output('live-graph', 'figure'),
-        [Input('graph-update', 'n_intervals')])
+@app.callback(  
+                Output('live-graph', 'figure'), # output() parameters: ID of the element, type of element with that ID
+                [Input('graph-update', 'n_intervals')] # create an input 'event' of type interval (n_intervals) with ID graphupdate
+              )
 def update_graph_scatter(n):
     X.append(X[-1]+1)
     Y.append(Y[-1]+Y[-1]*random.uniform(-0.1,0.1))
@@ -37,7 +40,7 @@ def update_graph_scatter(n):
             y=list(Y),
             name='Scatter',
             mode= 'lines+markers'
-            )
+        )
 
     return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
                                                 yaxis=dict(range=[min(Y),max(Y)]),)}
